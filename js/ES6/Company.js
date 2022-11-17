cddbf344d67570cfafee63d8be4e4d2c3b3fc62e
phamvinhquang97 <phamvinhquang77@gmail.com>
2022-11-17 11:51:37 +0930

    Add function delete, edit and find employee based on name

--- js/ES6/Company.js
+++ js/ES6/Company.js
@@ -13,6 +13,16 @@ class Company{
 
     }
 
+    // Find employee based on employeeID -> return location of employee in employeeList
+    findIndexEmployeeInEmployeeList(employeeID){
+        for(let index in this.employeeList){
+            if(this.employeeList[index].employeeID === employeeID){
+                return index;
+                break;
+            }
+        }
+    }
+
     // Find employee based on employeeID -> return location employee in employeeList
     findEmployeeInEmployeeList(employeeID){
         for(let index in this.employeeList){
@@ -21,6 +31,39 @@ class Company{
                 break;
             }
         }
+    }
+
+    // Delete employee based on employeeID
+    deleteEmployee(employeeID){
+        let indexInEmployeeList = this.findIndexEmployeeInEmployeeList(employeeID);
+        this.employeeList.splice(indexInEmployeeList, 1);
+    }
+
+    // Edit employee detail - Find index employee in employeeList -> edit detail
+    editEmployee(employee){
+        let indexInEmployeeList = this.findIndexEmployeeInEmployeeList(employee.employeeID);
+        this.employeeList[indexInEmployeeList] = employee;
+    }
+
+    // Find employee based on employee name -> employeeList
+    findEmployeeBasedOnName(searchName){
+        let employeeListResult = new Company();
+        // trim name string and make it uppercase
+        searchName = searchName.trim().toUpperCase();
+
+        // for ... of -> take the object in employeeList
+        for(let employee of this.employeeList){
+            let name = employee.name.trim().toUpperCase();
+            
+        
+            if(name.search(searchName) !== -1){
+                // return employeeList that was found
+                employeeListResult.employeeList = [...employeeListResult.employeeList, employee];
+            }
+        }
+        return employeeListResult;
+
+
     }
 
     
--- js/ES6/Main.js
+++ js/ES6/Main.js
@@ -1,5 +1,6 @@
-var newEmployee1 = new Employee("abc", "John", "john@gmail.com","123","12/11/2022", "staff");
-var newEmployee2 = new Employee("bcd", "Jack", "jack@gmail.com","123","12/11/2022", "staff");
+var newEmployee1 = new Employee("abc", " Tim John", "john@gmail.com","123","12/11/2022", "staff");
+var newEmployee2 = new Employee("bcd", "Jack Denials", "jack@gmail.com","123","12/11/2022", "staff");
+var newEmployee1_1 = new Employee("abc", "Jim", "jack@gmail.com","123","12/11/2022", "staff");
 var newCompany = new Company();
 newCompany.addNewEmployee(newEmployee1);
 newCompany.addNewEmployee(newEmployee2);
